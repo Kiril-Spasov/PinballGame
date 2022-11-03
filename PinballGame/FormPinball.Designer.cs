32b8a16b31f6869626a664413bd278283ea54c5d
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-03 15:56:57 +0200

    Add project files.

---
+++ PinballGame/FormPinball.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PinballGame
+{
+    partial class FormPinball
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(78, 80);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(220, 58);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start Simulation";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(465, 81);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(273, 306);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormPinball
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "FormPinball";
+            this.Text = "Pinball";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ PinballGame/FormPinball.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PinballGame
+{
+    public partial class FormPinball : Form
+    {
+        public FormPinball()
+        {
+            InitializeComponent();
+        }
+
+
+        //Our pinball machine has 6 pockets.
+        //We would like to know how many balls end up in each pocket
+        //after a large number of trials.
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            int[] frequency = new int[8];
+            Initialize(frequency, 7);
+            Simulate(frequency);
+            Display(frequency);
+            
+        }
+
+        private void Initialize(int[] f, int u)
+        {
+            for (int i = 1; i <= u; i++)
+            {
+                f[i] = 0;
+            }
+        }
+
+        private void Simulate(int[] f)
+        {
+            Random r = new Random();
+
+            double move;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                move = 4.0;
+                for (int j = 1; j <= 6; j++)
+                {
+                    int temp = r.Next(1, 3);
+                    if (temp == 1)
+                    {
+                        move += 0.5;
+                    }
+                    else
+                    {
+                        move -= 0.5;
+                    }
+                }
+
+                int c = (int)move;
+                f[c] += 1;
+            }
+        }
+
+        private void Display(int[] f)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                TxtResult.Text += "Pocket " + i + " frequency " + f[i] + Environment.NewLine;
+            }
+        }
+    }
+}
